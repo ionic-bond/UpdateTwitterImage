9a55c35950a48b71900584124190e30d9da9b10c
ionic-bond <ionicbond3@gmail.com>
2018-06-19 22:55:55 +0800

    Add files via upload

---
+++ RenameImage.py
@@ -0,0 +1,23 @@
+import os
+
+def RenameImg(dir_path, file_name):
+    after_point = file_name.split('.')[-1]
+    after__ = after_point.split('_')[-1]
+    if after__ == "orig":
+        old_path = os.path.join(dir_path, file_name)
+        new_path = os.path.join(dir_path, file_name[:-5])
+        os.rename(old_path, new_path)
+        print new_path
+
+def ScanDir(dir_path):
+    for file_name in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file_name)
+        if os.path.isdir(file_path):
+            ScanDir(file_path)
+        else:
+            RenameImg(dir_path, file_name)
+
+
+if __name__ == '__main__':
+    ScanDir(r'F:\Image')
+    os.system("pause")
